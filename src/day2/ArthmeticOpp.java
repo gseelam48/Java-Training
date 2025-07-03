881f06d1589c0e25a080b4ef6c442347dd1c1f0c
gseel <gseelam48@gmail.com>
2025-07-03 18:57:10 -0500

    ArthmeticOppDay2

---
+++ src/day2/ArthmeticOpp.java
@@ -0,0 +1,18 @@
+package day2;
+
+public class ArthmeticOpp {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int add = num1 + num2;               // pascal EmployerDetails,Salary
+        int sub = num1 - num2;               // camel  read(),readerName(),nextLine()
+        int mult = num1 * num2;
+        int div = num1 / num2;
+        System.out.println("Arthmetic Operations\n:" + add);
+        System.out.println(":" + sub);
+        System.out.println(":" + mult);
+        System.out.println(":" + div);
+        System.out.println(":" + div);
+
+    }
+}
