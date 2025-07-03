9c35a442f879245072fc4486bd9387e29a10a57f
gseel <gseelam48@gmail.com>
2025-07-03 18:58:06 -0500

    AddDay2

---
+++ src/day2/Add.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class Add {
+    public static void main(String[] args) {
+        byte byteval = 10;
+        short shortval = 20;
+        int intResult = byteval + shortval;
+        byte byteresult = (byte) (intResult);
+
+        System.out.println("integer int :" + byteresult);
+    }
+}
