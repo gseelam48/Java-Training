267a12a49502c0324ca641a5cfafbb9acd2b94c7
gseel <gseelam48@gmail.com>
2025-07-03 18:53:58 -0500

    PoemDay1

---
+++ src/day1/Poem.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Poem {
+    public static void main(String[] args) {
+        System.out.println("jhonny jhonny yes papa");
+        System.out.println("Eating suger no papa");
+        System.out.println("Telling lies no papa");
+        System.out.println("Open your mouth hahah..");
+    }
+
+}
