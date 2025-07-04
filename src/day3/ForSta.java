e0d443a777456609676c8c682a25fa604e041671
gseel <gseelam48@gmail.com>
2025-07-03 19:02:16 -0500

    ForStaDay3

---
+++ src/day3/ForSta.java
@@ -0,0 +1,9 @@
+package day3;
+
+public class ForSta {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 4; i++) {
+            System.out.println("hi" + i);
+        }
+    }
+}
