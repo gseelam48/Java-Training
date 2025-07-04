ae49d6995a5c286f48a4ca104b00a3f817ee06c0
gseel <gseelam48@gmail.com>
2025-07-03 19:03:15 -0500

    MethodsExaDay3

---
+++ src/day3/MethodsExa.java
@@ -0,0 +1,20 @@
+package day3;
+
+
+public class MethodsExa {
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+        int result = addtionof2Numbers(num1, num2);
+        System.out.println("add of two nums:" + result);
+    }
+
+    public static int addtionof2Numbers(int num1, int num2)
+    {
+
+        return num1 + num2;
+    }
+}
+
+
+
