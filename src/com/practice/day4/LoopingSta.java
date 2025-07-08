9ea1710018665bb2615cba552c2b84ab23ef6664
gseel <gseelam48@gmail.com>
2025-07-07 21:13:06 -0500

    LoopingStatDay4

---
+++ src/com/practice/day4/LoopingSta.java
@@ -0,0 +1,10 @@
+package com.practice.day4;
+
+public class LoopingSta {
+    public static void main(String[] args){
+
+        for (int i=1; i<=10; i++){
+        System.out.println("print1-10: " +i);
+    }
+}
+}
