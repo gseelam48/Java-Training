24e62407c873573100ebcb8981c549bb90581090
gseel <gseelam48@gmail.com>
2025-07-07 21:12:20 -0500

    EvenNumberWhileDay4

---
+++ src/com/practice/day4/EvenNumbersWhi.java
@@ -0,0 +1,11 @@
+package com.practice.day4;
+
+public class EvenNumbersWhi{
+    public static void main(String[] args) {
+        int i = 2;
+        while (i <= 20) {
+            System.out.println("print only even numbers from 1 to 20  :" + i);
+            i += 2;
+        }
+    }
+}
