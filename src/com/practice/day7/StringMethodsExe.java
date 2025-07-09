530838361c66740a4ad83db1bfe6492bbd6095e5
gseel <gseelam48@gmail.com>
2025-07-08 22:02:31 -0500

    StringMethodEx1Day7

--- src/com/practice/day7/StringMethodsExe.java
+++ src/com/practice/day7/StringMethodsExe.java
@@ -1,32 +1,30 @@
 package com.practice.day7;
 
-import java.util.Locale;
-
 public class StringMethodsExe {
     public static void main(String[] args) {
         String liquid = "Water";
-        String presence = "  WHITE printer  ";
+        String Appearance = "  WHITE printer  ";
         int length = liquid.length();
-        char charAt = presence.charAt(8);
-        String substring = presence.substring(4, 8);
-        boolean equals = presence.equals("  white prinTer  ");
-        boolean equalsIgnoreCase = presence.equalsIgnoreCase("  whIte printEr  ");
-        String lowerCase = presence.toLowerCase();
-        String uppercase = presence.toUpperCase();
-        String trim = presence.trim();
-        boolean contains = presence.contains("W");
-        String replace = presence.replace("WHITE", "white");
+        char charAt = Appearance.charAt(8);
+        String substring = Appearance.substring(4, 8);
+        boolean equals = Appearance.equals("  white prinTer  ");
+        boolean equalsIgnoreCase = Appearance.equalsIgnoreCase("  whIte printEr  ");
+        String lowerCase = Appearance.toLowerCase();
+        String uppercase = Appearance.toUpperCase();
+        String trim = Appearance.trim();
+        boolean contains = Appearance.contains("W");
+        String replace = Appearance.replace("WHITE", "white");
 
         System.out.println(" The length of given " + liquid + " is " + length);
-        System.out.println(" The  given " + presence + " is " + charAt);
-        System.out.println(" The  given " + presence + " is " + substring);
-        System.out.println(" The  given " + presence + " is " + equals);
-        System.out.println(" The  given " + presence + " is " + equalsIgnoreCase);
-        System.out.println(" The  given " + presence + " is " + lowerCase);
-        System.out.println(" The  given " + presence + " is " + uppercase);
-        System.out.println(" The  given " + presence + " is " + trim);
-        System.out.println(" The  given " + presence + " is " + contains);
-        System.out.println(" The  given " + presence + " is " + replace);
+        System.out.println(" The  given " + Appearance + " is " + charAt);
+        System.out.println(" The  given " + Appearance + " is " + substring);
+        System.out.println(" The  given " + Appearance + " is " + equals);
+        System.out.println(" The  given " + Appearance + " is " + equalsIgnoreCase);
+        System.out.println(" The  given " + Appearance + " is " + lowerCase);
+        System.out.println(" The  given " + Appearance + " is " + uppercase);
+        System.out.println(" The  given " + Appearance + " is " + trim);
+        System.out.println(" The  given " + Appearance + " is " + contains);
+        System.out.println(" The  given " + Appearance + " is " + replace);
 
     }
 }
