12c4a83bd73c422536f3b43f3f8357300f1ba4f7
gseel <gseelam48@gmail.com>
2025-07-09 19:50:01 -0500

    ArrayEx1Day8

---
+++ src/com/practice/day8/Array2.java
@@ -0,0 +1,16 @@
+package com.practice.day8;
+
+public class Array2 {
+    public static void main(String[] args) {
+        String[] x = new String[5];
+        x[0] = "go pi";
+        x[1] = "reddy";
+        x[2] = "water";
+        x[3] = "bottle";
+        x[4] = "cap";
+
+        for (int i = 0; i<x.length; i++) {
+            System.out.println(x[i]);
+        }
+    }
+}
