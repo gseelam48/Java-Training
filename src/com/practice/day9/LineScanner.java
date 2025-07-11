90e22e4c22618449393d77f754e76c72e1dab4d7
gseel <gseelam48@gmail.com>
2025-07-11 09:08:33 -0500

    ScannerLongDay9

---
+++ src/com/practice/day9/LineScanner.java
@@ -0,0 +1,14 @@
+package com.practice.day9;
+
+import java.util.Scanner;
+
+public class LineScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name1 = scanner.nextLine();
+        System.out.println(name1);
+    }
+}
+
+
+
