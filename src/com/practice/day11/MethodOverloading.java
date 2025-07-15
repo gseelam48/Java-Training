87043feaa138c94feb7076cb8d498f21e1d19733
gseel <gseelam48@gmail.com>
2025-07-14 21:25:31 -0500

    MethodOverLoadingConsDay11

--- src/com/practice/day11/MethodOverloading.java
+++ src/com/practice/day11/MethodOverloading.java
@@ -3,6 +3,36 @@ package com.practice.day11;
 import java.util.Scanner;
 
 public class MethodOverloading {
+    int a;
+    int b;
+
+    public int getA() {
+        return a;
+    }
+
+    public int setA(int a) {
+        this.a = a;
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public int setB(int b) {
+        this.b = b;
+        return b;
+    }
+
+    MethodOverloading() {
+
+    }
+
+    MethodOverloading(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
@@ -11,11 +41,19 @@ public class MethodOverloading {
 
 
         MethodOverloading methodOverloading = new MethodOverloading();
-
+        MethodOverloading methodOverloading1 = new MethodOverloading(a, b);
+        int y = methodOverloading.getB();
+        System.out.println(y);
+        int x = methodOverloading.getA();
+        System.out.println(x);
         int add = methodOverloading.add(a, b);
         System.out.println(add);
         int add1 = methodOverloading.add(a, b, c);
         System.out.println(add1);
+        int z = methodOverloading1.setA(a);
+        System.out.println(z);
+        int w = methodOverloading1.setB(b);
+        System.out.println(w);
 
     }
 
