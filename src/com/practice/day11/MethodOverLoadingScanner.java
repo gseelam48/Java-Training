00c5bb66f5323dcb1e12d37fb7d9e8aadc846588
gseel <gseelam48@gmail.com>
2025-07-14 21:42:07 -0500

    MethodOverLoadingScanner Day11

---
+++ src/com/practice/day11/MethodOverLoadingScanner.java
@@ -0,0 +1,30 @@
+package com.practice.day11;
+
+import java.util.Scanner;
+
+public class MethodOverLoadingScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+
+        MethodOverloading methodOverloading = new MethodOverloading();
+
+        int add = methodOverloading.add(a, b);
+        System.out.println(add);
+        int add1 = methodOverloading.add(a, b, c);
+        System.out.println(add1);
+    }
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
+
