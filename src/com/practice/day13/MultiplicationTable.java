ccf1ddbc3536ddf854174eb4deb2e1ebfac806f3
gseel <gseelam48@gmail.com>
2025-07-16 21:17:52 -0500

    MultiplicationTableDay13

---
+++ src/com/practice/day13/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package com.practice.day13;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " * " + i + " = " + num * i);
+        }
+    }
+}
+
