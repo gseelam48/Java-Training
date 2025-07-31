71884777eacb2a2804a21e8a464f2129532ada37
gseel <gseelam48@gmail.com>
2025-07-31 10:20:03 -0500

    ListSetExampleDay21

---
+++ src/com/practice/day21/ListSetExample.java
@@ -0,0 +1,27 @@
+package com.practice.day21;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ListSetExample {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(11);
+        list.add(23);
+        list.add(55);
+        list.add(66);
+        System.out.println(list.size());
+        System.out.println(list.get(2));
+
+        Set<Integer> set = new HashSet();
+        set.add(1);
+        set.add(2);
+
+
+
+    }
+
+
+}
