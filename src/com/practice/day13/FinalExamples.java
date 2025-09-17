ffa5e1fb21b3e70b5d2010429ff02ef7dfd72cd8
gseel <gseelam48@gmail.com>
2025-09-17 10:12:18 -0500

    FinalExam

---
+++ src/com/practice/day13/FinalExamples.java
@@ -0,0 +1,9 @@
+package com.practice.day13;
+
+public class FinalExamples {
+    public static void main(String[] args) {
+      int a = 20;
+        a = 20;
+        System.out.println(a);
+    }
+}
