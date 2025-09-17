c2971899a559e3befda95c9cc2b8859ffac7f11d
gseel <gseelam48@gmail.com>
2025-09-17 10:16:26 -0500

    Methodsinobjectiveclass

---
+++ src/com/practice/day19/MethodsInObjectClass.java
@@ -0,0 +1,27 @@
+package com.practice.day19;
+
+import java.awt.print.Book;
+
+/*public class MethodsInObjectClass {
+    public static void main(String[] args) {
+        // with the help of constructor
+        System.out.println("with the help of book");
+        Book book = new Book();
+        Library library = new Library(book);
+        Library.printBookDetails();
+        // with the help setter and getter
+        System.out.println("with the help of setters and getters");
+        Book book1 = new Book();
+        book1.setTitle("java");
+        book1.setAuthor("james gosslings");
+
+        Library library1 = new Library();
+        library1.setBook(book1);
+        library1.printBookDetails();
+        Book book2 = library1.getBook();
+        book2.wait();
+        System.out.println(book2.getClass());
+
+    }
+}
+*/
